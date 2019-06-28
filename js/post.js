ee2b560461d8757279d5dc44d1f18d59be551e99
sbhalla98 <sonalibhalla98@gmail.com>
2019-06-28 22:56:20 +0530

    post page completed

--- js/post.js
+++ js/post.js
@@ -1,3 +1,40 @@
-function editable(id){
+document.getElementById("commentBox").style.display = "none";
+function editable(){
+    
     document.getElementById("blogBody").contentEditable = "true";
+    document.getElementById("blogTitleNew").contentEditable = "true";
+    document.getElementById("bedit").innerHTML =   '<button class="btn" id="edit" onclick="noneditable(blogBody)">'+"Save" +"&nbsp<i class='material-icons' style='font-size: 15px;'>save</i></button>";
+  
+
 }
+function noneditable(){
+
+    document.getElementById("blogBody").contentEditable = "false";
+    document.getElementById("blogTitleNew").contentEditable = "false";
+    document.getElementById("bedit").innerHTML =  '<button class="btn" id="edit" onclick="editable(blogBody)">Edit &nbsp<i class="material-icons" style="font-size: 15px;">edit</i></button> ';
+
+}
+var c = 0;
+function like(){
+  c++;
+  if(c==1){
+  document.getElementById("likestatement").innerHTML = c + " person likes this!";
+  }
+  else{
+    document.getElementById("likestatement").innerHTML = c + " persons have liked this!";
+  }
+
+}
+var x = document.getElementById("text").defaultValue;
+function comment(id){
+    document.getElementById("nocomment").style.display = "none";
+    document.getElementById("commentBox").style.display = "block";
+    if(id.value == ""){
+        alert('!invalid comment!');
+    }
+    else{
+ document.getElementById("t01").innerHTML += '<div style="background-color:white;padding:10px;margin:1%;margin-bottom:0px;">'+id.value+'</div>';
+ document.getElementById("text").value = x; 
+    }   
+}
+
